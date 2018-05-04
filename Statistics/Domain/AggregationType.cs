fd2232de95a94213483058b376952bf8db972223
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-04 22:02:30 +0900

    Implemented the AggregtionType enum.

---
+++ Statistics/Domain/AggregationType.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain
+{
+    public enum AggregationType
+    {
+        Mean,
+        Mode
+    }
+}
---
+++ Statistics/Tests/Domain.Tests/AggregationTypeTestShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+namespace Domain.Tests
+{
+    public class AggregationTypeTestShould
+    {
+        [Fact]
+        public void RetrieveMeanTypeAsZero()
+        {
+            // Act
+            var mean = AggregationType.Mean;
+
+            // Assert
+            // How to convert from System.Enum to base integer?
+            // https://stackoverflow.com/questions/908543/how-to-convert-from-system-enum-to-base-integer
+            Assert.Equal(0, Convert.ToInt32(mean));
+        }
+
+        [Fact]
+        public void RetrieveModeTypeAsOne()
+        {
+            // Act
+            var mode = AggregationType.Mode;
+
+            // Assert
+            Assert.Equal(1, Convert.ToInt32(mode));
+        }
+    }
+}
