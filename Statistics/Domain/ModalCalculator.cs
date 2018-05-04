005108318fec409296f46d7bb7e1b612afe5a9f4
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-05 02:29:35 +0900

    Implemented the ModalCalculator class.

---
+++ Statistics/Domain/ModalCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Domain.Tests
+{
+    public class ModalCalculator
+    {
+        public Measurement Aggregate(IEnumerable<Measurement> measurements)
+        {
+            return new Measurement
+            {
+                HighValue = CalculateMode(measurements, m => m.HighValue),
+                LowValue = CalculateMode(measurements, m => m.LowValue),
+            };
+        }
+
+        private double CalculateMode(IEnumerable<Measurement> measurements,
+            Func<Measurement,double> keySelector)
+        {
+            double value = double.NaN;
+            var valueQuery = measurements.GroupBy(keySelector)
+                .OrderByDescending(g => g.Count())
+                .Select(g => new
+                {
+                    Value = g.Key,
+                    Count = g.Count(),
+                });
+            var valueOne = valueQuery.FirstOrDefault();
+            var valueTwo = valueQuery.Skip(1).FirstOrDefault();
+            if (valueOne == null)
+            {
+                value = double.NaN;
+            }
+            else if (valueTwo == null)
+            {
+                value = valueOne.Value;
+            }
+            else if (valueOne.Count == valueTwo.Count)
+            {
+                value = double.NaN;
+            }
+            else
+            {
+                value = valueOne.Value;
+            }
+            return value;
+        }
+    }
+}
---
+++ Statistics/Tests/Domain.Tests/ModalCalculatorTestShould.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Domain.Tests
+{
+    public class ModalCalculatorTestShould
+    {
+        [Theory]
+        [MemberData(nameof(GetMeasurements))]
+        public void CalculateMode(IEnumerable<Measurement> measurements,
+            double expectedModeHighValue, double expectedModeLowValue)
+        {
+            // Arrange
+            var modalCalc = new ModalCalculator();
+
+            // Act
+            var actual = modalCalc.Aggregate(measurements);
+
+            // Assert
+            Assert.Equal(expectedModeHighValue, actual.HighValue);
+            Assert.Equal(expectedModeLowValue, actual.LowValue);
+        }
+
+        public static IEnumerable<object[]> GetMeasurements()
+        {
+            return new List<object[]>
+            {
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 10.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 5.0, LowValue = 2.0},
+                        new Measurement() {HighValue = 2.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 10.0, LowValue = 4.0},
+                    },
+                    10.0, 1.0
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 10.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 5.0, LowValue = 2.0},
+                        new Measurement() {HighValue = 2.0, LowValue = 1.0},
+                    },
+                    double.NaN, 1.0
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 10.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 5.0, LowValue = 2.0},
+                        new Measurement() {HighValue = 10.0, LowValue = 4.0},
+                    },
+                    10.0, double.NaN
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 10.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 2.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 10.0, LowValue = 4.0},
+                    },
+                    10.0, 1.0
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 5.0, LowValue = 2.0},
+                        new Measurement() {HighValue = 2.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 10.0, LowValue = 4.0},
+                    },
+                    double.NaN, double.NaN
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 10.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 5.0, LowValue = 2.0},
+                    },
+                    double.NaN, double.NaN
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 10.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 2.0, LowValue = 1.0},
+                    },
+                    double.NaN, 1.0
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 5.0, LowValue = 2.0},
+                        new Measurement() {HighValue = 2.0, LowValue = 1.0},
+                    },
+                    double.NaN, double.NaN
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 10.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 10.0, LowValue = 4.0},
+                    },
+                    10.0, double.NaN
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 5.0, LowValue = 2.0},
+                        new Measurement() {HighValue = 10.0, LowValue = 4.0},
+                    },
+                    double.NaN, double.NaN
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 2.0, LowValue = 1.0},
+                        new Measurement() {HighValue = 10.0, LowValue = 4.0},
+                    },
+                    double.NaN, double.NaN
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 10.0, LowValue = 1.0},
+                    },
+                    10.0, 1.0
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 5.0, LowValue = 2.0},
+                    },
+                    5.0, 2.0
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 2.0, LowValue = 1.0},
+                    },
+                    2.0, 1.0
+                },
+                new object[]
+                {
+                    new List<Measurement>
+                    {
+                        new Measurement() {HighValue = 10.0, LowValue = 4.0},
+                    },
+                    10.0, 4.0
+                },
+            };
+        }
+    }
+}
