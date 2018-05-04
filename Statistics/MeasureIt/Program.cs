42baf6b7bc7b7761ffc4781250784d22b46ede87
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-05 03:05:30 +0900

    Added a project to demostrate this video clip.

---
+++ Statistics/MeasureIt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeasureIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
